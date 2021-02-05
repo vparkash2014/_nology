452cd0f194a7bd9dd4f8ef2517228161eff897dd
Vaishali Parkash <vaishaliparkash@MacBook.local>
2021-02-05 18:14:21 +1100

    adding week1 lessons

---
+++ precourse/html_lesson/create_files.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+touch 04B_mini_challenge.html
+touch 05B_mini_challenge.html
+touch 06B_mini_challenge.html
+touch 07B_mini_challenge.html
+touch 05A_css_combinators.html
+touch 06A_id_class.html
+touch 07A_box_model.html
+git add --a
+git commit --m "Create html files"
